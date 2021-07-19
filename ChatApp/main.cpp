b5209a3620a444068e7bb0d2e2b611a206ca498f
jbuckl8 <jbuckl8@uwo.ca>
2021-07-19 11:10:05 -0400

    Initial setup

---
+++ ChatApp/main.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <WS2tcpip.h>
+
+#pragma comment (lib, "ws2_32.lib")
+
+using namespace std;
+
+void main() {
+
+	// Initalize winsock
+	
+	// Create a socket
+
+	// Bind the socket to an ip address and port
+
+	// Tell winsock the socket is for listening
+
+	// wait for a connection
+
+	// close listening socket
+
+	// while loop: accept and echo message back to client
+
+	// close the sock
+
+	// Shutdown winsock
+
+
+
+
+
+
+}
